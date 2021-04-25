c3a0c28ea5117a8974866e0d752da06735a22441
singek101 <michaelmusediku@gmail.com>
2021-04-25 21:52:22 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace week2_lab_assignment
+{
+    class Program
+    {
+        public static void  remainder(int num1, int num2)
+        {
+            if (num1 < num2)
+            {
+                Console.WriteLine(num2 % num1);
+            }
+            else
+            {
+                Console.WriteLine(num1 % num2);
+            }
+        }
+        static void Main(string[] args)
+        {
+         Program.remainder(25,30); //calling the static class member.
+        }
+    }
+}
